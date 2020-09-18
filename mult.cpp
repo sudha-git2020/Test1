22591d74a6e69122e820e593886a4092f57c8bd1
sudha-git2020 <68510722+sudha-git2020@users.noreply.github.com>
2020-09-18 15:31:23 -0400

    Create mult.cpp

---
+++ mult.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int Mulitply(int a, int b) {
+	int c = a * b; 
+  return c; 
+}
+
+int main() {
+
+int result; 
+
+result = Mulitply(3,4);
+
+count << "Prodcut of 3,4 is " << result << endl; 
+
+}
